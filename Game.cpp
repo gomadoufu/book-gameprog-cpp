8cb8766b6e9407123e31e6e5c95106c5052574b2
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2024-09-01 01:21:28 +0900

    feat: とりあえず画面が出た！ 🥳

--- Game.cpp
+++ Game.cpp
@@ -5,4 +5,52 @@ Game::Game() {
     mIsRunning = true;
 }
 
-auto Game::Initialize() -> bool { auto sdlResult = SDL_Init(SDL_INIT_VIDEO); }
+auto Game::Initialize() -> bool {
+    int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+    if (sdlResult != 0) {
+        SDL_Log("SDLを初期化できません: %s", SDL_GetError());
+        return false;
+    }
+
+    mWindow = SDL_CreateWindow("ゲームプログラミングC++(第1章)", 100, 100, 1024,
+                               768, 0);
+    if (!mWindow) {
+        SDL_Log("ウィンドウの作成に失敗しました:%s", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+auto Game::Shutdown() -> void {
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
+}
+
+auto Game::RunLoop() -> void {
+    while (mIsRunning) {
+        ProcessInput();
+        UpdateGame();
+        GenerateOutput();
+    }
+}
+
+auto Game::ProcessInput() -> void {
+    SDL_Event event;
+    // キューにイベントがあれば繰り返す
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            // SDL_QUIT イベントならば、ループを終える
+            case SDL_QUIT:
+                mIsRunning = false;
+                break;
+        }
+    }
+    // キーボードの状態を取得する
+    const auto state = SDL_GetKeyboardState(NULL);
+    // [ESC] キーが押された時も、ループを終える
+    if (state[SDL_SCANCODE_ESCAPE]) {
+        mIsRunning = false;
+    }
+}
+auto Game::UpdateGame() -> void { return; }
+auto Game::GenerateOutput() -> void { return; }
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "Game.h"
+
+int main(int argc, char** argv) {
+    Game game;
+    auto success = game.Initialize();
+    if (success) {
+        game.RunLoop();
+    }
+    game.Shutdown();
+    return 0;
+}
